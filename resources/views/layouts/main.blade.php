02a6311d283806a3e6f94e2d847dccca6181dc0d
Rofi <rofinb123@gmail.com>
2023-01-22 12:41:32 +0700

    pushing

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    {{-- My Style --}}
+    <link rel="stylesheet" href="/css/style.css">
+
+    <!-- Navbar CSS -->
+    <link rel="stylesheet" href="/css/navbar.css">
+
+    @yield('style')
+    <link rel="shortcut icon" href="/img/logofooter.png">
+    <!-- Footer CSS -->
+    <link rel="stylesheet" href="/css/footer.css">
+
+    <title>HMIK | {{ $title }}</title>
+</head>
+
+<body>
+
+    @include('partials.navbar')
+
+    @yield('container')
+
+    @include('partials.footer')
+</body>
+
+</html>
